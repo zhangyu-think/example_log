4cacb62658bb527c66681bc2c9d03a16b44e2d92
zhangyu-think <38775332+zhangyu-think@users.noreply.github.com>
2018-08-15 17:56:55 +0800

    5

--- src/main/java/top/qifansfc/example_log/Test.java
+++ src/main/java/top/qifansfc/example_log/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static Logger LOGGER = Logger.getLogger(Test.class);
 	public static void main(String[] args) {
 		LOGGER.debug("test info");
-		LOGGER.info("test info");
+		LOGGER.info("test info1");
 		LOGGER.error("test error");
 	}
 }
