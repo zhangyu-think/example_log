bc4ac9a96595d8fd3b3128000da4afe66a9a1e3f
zhangyu-think <38775332+zhangyu-think@users.noreply.github.com>
2018-08-15 17:52:24 +0800

    4

---
+++ src/main/java/top/qifansfc/example_log/Test.java
@@ -0,0 +1,12 @@
+package top.qifansfc.example_log;
+import org.apache.log4j.Logger;
+
+public class Test {
+
+	public static Logger LOGGER = Logger.getLogger(Test.class);
+	public static void main(String[] args) {
+		LOGGER.debug("test info");
+		LOGGER.info("test info");
+		LOGGER.error("test error");
+	}
+}
